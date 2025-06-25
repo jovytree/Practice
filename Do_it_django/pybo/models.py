12faa10e36311c7cc14cc5a3c9ad4a57333a4e1c
jovytree <jovytree@gmail.com>
2025-06-25 21:42:20 +0900

    django practice 2-부트스트랩

--- Do_it_django/pybo/models.py
+++ Do_it_django/pybo/models.py
@@ -7,7 +7,8 @@ class Question(models.Model):
     create_date = models.DateTimeField()
 
     def __str__(self):
-        return self.subject # __str__ 메서드를 추가해서 장고 셸에서 Question.objects.all()을 해서 데이터 조회 시 id가 아닌 제목을 표시하게 한다.
+        return self.subject
+    # __str__ 메서드를 추가해서 장고 셸에서 Question.objects.all()을 해서 데이터 조회 시 id가 아닌 제목을 표시하게 한다.
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
