ff7b163f016fd48060e186b819403267cc078b21
jovytree <jovytree@gmail.com>
2025-06-25 23:29:39 +0900

    장고 기초 공사 끝

--- Do_it_django/pybo/forms.py
+++ Do_it_django/pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 # 장고 폼은 2개의 폼으로 구분할 수 있다. froms.Form을 상속 받으면 폼.
 # forms.ModelForm을 상속받으면 모델 폼이라 부른다.
@@ -9,15 +9,18 @@ class QuestionForm(forms.ModelForm):
     class Meta: # 장고 모델 폼은 내부 클래스로 Meta 클래스를 반드시 가져야 한다.
         model = Question # 폼이 사용할 모델
         fields = ['subject', 'content'] # 필드들을 적어야 한다.
-
-        # form.as_p 태그는 부트스트랩을 적용할 수 없다는 단점이 있다.
-        # 완벽하지는 않지만, widgets 속성을 아래와 같이 추가하면 해결할 수 있다.
-        widgets = {
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'}),
-        }
-
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+        # {{form.as_p}}는 디자인 측면에서 제한이 생긴다. 수작업으로 폼을 작성해보자
+        # widget을 삭제했다.
+
+# 답변 등록 기능에 장고 폼을 적용해보자.
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content' : '답변내용',
         }
